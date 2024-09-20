5fa999e70b273e9d09553cb408fde899756496e3
Yuxuan Xie <teststaybaka@gmail.com>
2024-09-20 13:07:22 -0700

    Allow empty fields in message. Release

--- message_generator.ts
+++ message_generator.ts
@@ -26,78 +26,75 @@ export function generateMessage(
   let fields = new Array<string>();
   let fieldDescriptors = new Array<string>();
   let usedIndexes = new Set<number>();
-  if (!messageDefinition.fields) {
-    throw new Error(
-      `${loggingPrefix} "fields" is either missing or not an array.`,
-    );
-  }
-  for (let field of messageDefinition.fields) {
-    if (!field.name) {
-      throw new Error(`${loggingPrefix} "name" is missing on a field.`);
-    }
-    if (!field.index) {
-      throw new Error(
-        `${loggingPrefix} "index" is missing on field ${field.name}.`,
-      );
-    }
-    if (usedIndexes.has(field.index)) {
-      throw new Error(
-        `${loggingPrefix} field ${field.name} has a duplicate index ${field.index}.`,
-      );
-    }
-    usedIndexes.add(field.index);
-
-    if (!field.type) {
-      throw new Error(`${loggingPrefix} "type" is missing on ${field.name}.`);
-    }
-    let descriptorLine: string;
-    if (PRIMITIVE_TYPES.has(field.type)) {
-      tsContentBuilder.importFromMessageDescriptor("PrimitiveType");
-      descriptorLine = `primitiveType: PrimitiveType.${field.type.toUpperCase()}`;
-    } else {
-      let definition = messageResolver.resolve(
-        loggingPrefix,
-        field.type,
-        field.import,
-      );
-      if (definition.enum) {
-        let enumDescriptorName = toUppercaseSnaked(field.type);
-        tsContentBuilder.importFromDefinition(
-          field.import,
-          field.type,
-          enumDescriptorName,
-        );
-        descriptorLine = `enumType: ${enumDescriptorName}`;
-      } else if (definition.message) {
-        let messageDescriptorName = toUppercaseSnaked(field.type);
-        tsContentBuilder.importFromDefinition(
-          field.import,
-          field.type,
-          messageDescriptorName,
-        );
-        descriptorLine = `messageType: ${messageDescriptorName}`;
-      } else {
+  if (messageDefinition.fields) {
+    for (let field of messageDefinition.fields) {
+      if (!field.name) {
+        throw new Error(`${loggingPrefix} "name" is missing on a field.`);
+      }
+      if (!field.index) {
         throw new Error(
-          `${loggingPrefix} a new definition needs to be handled for type ${field.type} of field ${field.name}.`,
+          `${loggingPrefix} "index" is missing on field ${field.name}.`,
         );
       }
-    }
-    let fieldTypeName: string;
-    let isArrayLine: string;
-    if (field.isArray) {
-      fieldTypeName = `Array<${field.type}>`;
-      isArrayLine = `isArray: true`;
-    } else {
-      fieldTypeName = field.type;
-    }
-    fieldDescriptors.push(`{
+      if (usedIndexes.has(field.index)) {
+        throw new Error(
+          `${loggingPrefix} field ${field.name} has a duplicate index ${field.index}.`,
+        );
+      }
+      usedIndexes.add(field.index);
+
+      if (!field.type) {
+        throw new Error(`${loggingPrefix} "type" is missing on ${field.name}.`);
+      }
+      let descriptorLine: string;
+      if (PRIMITIVE_TYPES.has(field.type)) {
+        tsContentBuilder.importFromMessageDescriptor("PrimitiveType");
+        descriptorLine = `primitiveType: PrimitiveType.${field.type.toUpperCase()}`;
+      } else {
+        let definition = messageResolver.resolve(
+          loggingPrefix,
+          field.type,
+          field.import,
+        );
+        if (definition.enum) {
+          let enumDescriptorName = toUppercaseSnaked(field.type);
+          tsContentBuilder.importFromDefinition(
+            field.import,
+            field.type,
+            enumDescriptorName,
+          );
+          descriptorLine = `enumType: ${enumDescriptorName}`;
+        } else if (definition.message) {
+          let messageDescriptorName = toUppercaseSnaked(field.type);
+          tsContentBuilder.importFromDefinition(
+            field.import,
+            field.type,
+            messageDescriptorName,
+          );
+          descriptorLine = `messageType: ${messageDescriptorName}`;
+        } else {
+          throw new Error(
+            `${loggingPrefix} a new definition needs to be handled for type ${field.type} of field ${field.name}.`,
+          );
+        }
+      }
+      let fieldTypeName: string;
+      let isArrayLine: string;
+      if (field.isArray) {
+        fieldTypeName = `Array<${field.type}>`;
+        isArrayLine = `isArray: true`;
+      } else {
+        fieldTypeName = field.type;
+      }
+      fieldDescriptors.push(`{
     name: '${field.name}',
     index: ${field.index},
     ${descriptorLine},${isArrayLine ? "\n    " + isArrayLine + "," : ""}
   }`);
-    if (!field.deprecated) {
-      fields.push(`
+      if (!field.deprecated) {
+        fields.push(`
   ${field.name}?: ${fieldTypeName},`);
+      }
     }
   }
 
